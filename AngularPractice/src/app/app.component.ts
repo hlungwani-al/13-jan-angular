ee490d14fcb0792a1f4700cab5be074d1e20b24d
hlungwani-al <hlungwani.al6@gmail.com>
2025-01-13 15:52:43 +0200

    ng model

---
+++ AngularPractice/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ FormsModule,
+    CommonModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'AngularPractice';
+  userName: String = '';
+  email:String = '';
+}
